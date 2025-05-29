0dc0b320fbfc9f1f4111c075297583e71dfe1b6c
mayuu-dks <mayuu.pp104@gmail.com>
2025-05-29 21:45:28 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
      <html lang="en" className={inter.className}>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+     <body className={`${inter.className} antialiased`}>
         {/* ← Client Component をここで呼び出す */}
         <DevToolsScript />
         {children}
